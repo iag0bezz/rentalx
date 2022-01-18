061b2ba4fe876899840442ee451b028334c5937a
Iago Beserra <iago.beserra1@gmail.com>
2022-01-18 20:28:21 -0300

    feat(users): implement avatar update

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
